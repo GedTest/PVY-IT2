f39e864b224f3eaac9c90c6b07cde4f09a46ec33
Viktor Zwinger <45140691+GedTest@users.noreply.github.com>
2020-06-04 14:54:01 +0200

    Upgrade

---
+++ Bosna_a_Hercegovina/JavaScript/historie.js
@@ -0,0 +1,140 @@
+const text = [{
+        "title": "Raný středověk",
+        "text": "Původně římskou provincii Ilyricum obsadili v 6.–7. století Slované. Území Bosny bylo nejdříve součástí Byzantské říše, později Chorvatského království, srbských království Duklji (Zety) a Rašky a Uher.",
+        "id": "0",
+        "path": "Stredovek_mapa.png"
+    },
+    {
+        "title": "Středověk",
+        "text": "Ve 12. století se Bosna za vlády bána Kulina de facto osamostatnila a odolávala snahám Srbska, Uher,Chorvatska a Benátek o její ovládnutí. Po roce 1353 nakrátko rozkvetla za bána, později krále, Tvrtka I.Kotromaniće. V roce 1463 byla Bosna dobyta Turky, následujícího léta některé části země obsadil uherský král Matyáš Korvín a vytvořil zde dvě bánoviny, stejně jako roku 1482 hornatější Hercegovina (Hum).",
+        "id": "1",
+        "path": "Ostrožac_hrad.jpg"
+    },
+    {
+        "title": "Osmanská Bosna",
+        "text": "Za vlády Osmanské říše mnoho bosenských křesťanů přešlo na islám, čímž později došlo ke vznikuspecifického etnika – bosenských Muslimů (Bosňáků). Stát byl rozdělen na bány (něco jako regiony) a zažívá mírnýprůmyslový rozvoj. Vyostřují se spory se sousedním Rakousko-Uherskem.",
+        "id": "2",
+        "path": "mostar_most.jpg"
+    },
+    {
+        "title": "Rakousko-Uherská vláda",
+        "text": "Roku 1875 se zejména pravoslavní Bosňané vzbouřili proti osmanské nadvládě. V roce 1878 se tak Bosna stala protektorátem Rakouska-Uherska. Rakouskouherská vojska vstoupila do Bosny a Hercegoviny 29. července 1878. Rakousko-Uhersko vládne zemi až do skončení první světové války, která začala atentátem na František Ferdinand d'Este v Sarajevu. Roku 1908 došlo k anexi Bosny a Hercegoviny Rakouskem-Uherskem.",
+        "id": "3",
+        "path": "atentat.jpg"
+    },
+    {
+        "title": "Království Jugoslávie",
+        "text": "V Království Srbů, Chorvatů a Slovinců vzniklém v r. 1918 (1929 přejmenovaném na Jugoslávii) byly bosenské národnosti zahrnuty do sjednoceného státu. Stát patřil mezi ty nejzaostalejší a v zemi bylo velké množství politických stran (podobně jako tomu bylo například v Československu). Došlo také k nepokojům, kdy se Srbové rozhodli pro odplatu vůči majetným Bosňákům.",
+        "id": "4",
+        "path": "kingdom_of_bosnia.png"
+    },
+    {
+        "title": "Druhá světová válka",
+        "text": "Během druhé světové války bylo Království Jugoslávie rozbito nacistickým Německem a oblast Bosny a Hercegoviny přičleněna k vzniklému Nezávislému státu Chorvatsko, jednomu ze satelitů Třetí říše. V Bosně a Hercegovině pod vládou Ustašovců tak začalo hromadné vyvražďování Srbů, Černohorců, Židů a Romů. Jako státní náboženství byl uznán jen římský katolicismus a také islám, zatímco pravoslaví jakožto symbol Srbů bylo tvrdě potlačováno. Během ustašovské nadvlády v koncentračních táborech a během čistek zahynulo jen v Bosně a Hercegovině asi 209000 Srbů a Černohorců. Proti režimu se zvedl organizovaný odpor tzv. Jugoslávští partyzáni v čele s Josipem Titem, ti podnikali záškodnické akce proti fašistům.",
+        "id": "5",
+        "path": "Partyzani.jpg"
+    },
+    {
+        "title": "Socialistická Jugoslávie",
+        "text": "V jugoslávské federaci (od r.1945) se stala Bosna a Hercegovina jednou ze 6 svazových republik, vymezenou na národnostním základě (s rovnoprávným postavením všech tří etnik). Jugoslávie, byla místem mnoha socialistických projektů, budovali se hlavně dopravní sítě, také se zde začalo združstevňovat. Socialistické zřízení nebylo mezi občany populární, a tak se Sedláci, ale i partyzáni bouřili.",
+        "id": "6",
+        "path": "yugoslavia_flag.jpg"
+    },
+    {
+        "title": "Válka v Bosně",
+        "text": "V době krize federace na začátku devadesátých let se mezi vedoucími představiteli bosenských Muslimů, Chorvatů a Srbů projevily rozpory ve věci formováni nezávislého státu. V březnu 1991 se sešli v Karadjordjevu ve Vojvodině Aliju Izetbegović, Slobodan Milošević a Franjo Tuđman, aby si dohodli rozdělení země. Bosňáci a Chorvati vyhlásili 3. března 1992 nezávislost Bosny a Hercegoviny. Srbové 7. Dubna 1992 vyhlásili vlastní republiku. Politický konflikt přerostl v ozbrojené srážky, kterým padlo za oběť mnoho obyvatel Bosny a Hercegoviny (mj. v tzv. etnických čistkách) a které velmi posílily vzájemnou nevraživost mezi národnostmi země. V této válce bojoval prakticky každý proti každému. OSN vyslala do Bosny a Hercegoviny jednotky, které rozdělily bojující strany. Nadace Lidových novin se významně podílela na humanitární pomoci pro Bosnu a Hercegovinu. Mezi lety 1995 až 1998 představoval celkový objem pomoci 150 milionů korun. Po 42 měsících bojů byla pod tlakem Spojených států amerických 21. listopadu v Daytonu uzavřena mírová dohoda, která byla slavnostně ratifikována všemi bojujícími stranami v Paříži 14. prosince 1995. V roce 2007 vstoupila Bosna a Hercegovina do Středoevropské zóny volného obchodu.",
+        "id": "7",
+        "path": "valka.jpg"
+    }
+];
+const obrazkyText = [{
+        "title": "Dragoljub Mihailović",
+        "text": "Dragoljub „Draža“ M. Mihailović známý též pod jménem Čiča, byl srbský generál. eho kariéra začala již za první světové války, kdy se stal válečným hrdinou. V meziválečné královské armádě pak zastával hodnost plukovníka. V druhé světové válce se stal vůdcem četnických sborů, které bojovaly proti komunistickým partyzánům, ale i nacistické okupaci. Tehdejší německou správu účast Četniků, kteří získávali pomoc ze zahraničí, značně znepokojovala, a proto se pokoušeli Michaloviće zastavit. Jeho odsouzení a popravu však již vykonal komunistický režim, který byl vybudován po úspěchu partyzánského boje pod vedením Tita.",
+        "id": "0"
+    },
+    {
+        "title": "Petar Kočić",
+        "text": "Petar Kočić byl bosenskohercegovský básník, spisovatel a politik srbského původu. Kočić pocházel z regionu Banja Luky, nejprve se učil v klášteře, později pokračoval ve středoškolském studiu v Sarajevu. Musel však ve třetím ročníku odejít, a to do Bělehradu, kde svoji školu dokončil. V Sarajevu, kde byl činovníkem nakladatelství Prosveta, ale po nedlouhé době byl propuštěn, neboť se účastnil stávky dělníků. Poté, co byla Bosna a Hercegovina roku 1908 anektována Rakousko-Uherskem, Kočić založil noviny Otadžbina (Vlast) a začal iniciovat politickou skupinu, která hlásala boj jak okupačnímu Rakousku-Uhersku. Jako národní a sociální revolucionář získal Kočić poměrně velkou popularitu. Kočić napsal tři sbírky povídek, kterými jsou S planine i ispod planine (Z hor a podhůří), Jauci sa Zmijanja (Sténání ze Zmijanje) a dvě politicko-sociální satiry: Jazevac pred sudom (Jezevec před soudem) a Sudanija (Souzení). V současné době je Petar Kočić vyobrazen na bosenskohercegovské stomarkové bankovce, kterou vydává Republika srbská.",
+        "id": "1"
+    },
+    {
+        "title": "František Ferdinand d'Este",
+        "text": "František Ferdinand Karel Ludvík Josef Maria arcivévoda Rakouský-Este byl následník rakousko-uhrského trůnu a synovec císaře a krále Františka Josefa I. V zemích Koruny české vlastnil panství a zámky Konopiště a Chlum u Třeboně. Dne 28. června 1914 byl v Sarajevu na něj a jeho manželku vévodkyni Žofii z Hohenbergu spáchán atentát, kterému oba manželé podlehli. Tento útok se stal záminkou pro začátek první světové války v Evropě.",
+        "id": "2"
+    },
+    {
+        "title": "Dr. Mehmed Spaho",
+        "text": "Mehmed Spaho byl bosenskohercegovský politik bosňáckého původu. Mezi dvěma světovými válkami byl klíčovou politickou osobností politického života muslimů v Bosně a Hercegovině",
+        "id": "3"
+    },
+    {
+        "title": "Josip Broz Tito",
+        "text": "Josip Broz Tito byl prezident Socialistické federativní republiky Jugoslávie, předseda Svazu komunistů Jugoslávie, maršál Jugoslávie a vrchní velitel ozbrojených sil SFRJ a vůdce jugoslávského protinacistického odboje za druhé světové války. Po válce patřil mezi zakládající členy kominformy, po roztržce se Sovětským svazem stál při zrodu Hnutí nezúčastněných zemí, jehož hlavním představitelem byl až do své smrti.",
+        "id": "4"
+    }
+];
+
+let current = null;
+
+$(function () {
+
+    $("#osobnosti img").hide();
+
+    /*$("thead tr th").on("click", function () {
+        text.forEach(e => {
+            $("#historie h2").hide();
+            $("#historie p").hide();
+            $("#historie img").hide();
+        });
+
+        current = $(this);
+
+        text.forEach(e => {
+            if (current[0].id == e.id) {
+                $("#historie").append(`<h2 class="text-center" style="width: 50%; margin-left: 25%">${e.title}</h2><p>${e.text}</p><img src="img/${e.path}" alt="${e.title}" style="width 100%">`);
+                $("#historie img").animate({opacity: '0.1'}).delay(100).animate({
+                    opacity: '1'
+                });
+                $("#historie h2").delay(3000).slideUp(1500);
+            }
+        });
+    });*/
+/*
+    $("thead tr th").mouseover(function () {
+        $(this).css({
+            "color": "rgb(255,204,0)",
+            "width": "225",
+            "background-color": "rgb(0, 0, 160)"
+        });
+    });
+    $("thead tr th").mouseout(function () {
+        $(this).css({
+            "color": "black",
+            "width": "145",
+            "background-color": "rgb(212, 231, 255)"
+        });
+    });*/
+    $("h1:eq(2)").mouseover(function () {
+        $(this).animate({
+            fontSize: '50px',
+            marginLeft: '33%',
+            width: '400px'
+        }, 300);
+        $("#osobnosti img").show(500, "swing");
+    });
+
+
+    $("#osobnosti img").mouseover(function () {
+        current = $(this);
+
+        obrazkyText.forEach(e => {
+            if (current[0].alt == e.title) {
+                $(`#osobnosti p:eq(${e.id})`).text(e.text);
+                $(`#osobnosti p:eq(${e.id})`).prepend(`<h2>${e.title}</h2>`);
+            } else {
+                $(`#osobnosti p:eq(${e.id})`).text("");
+                $(`#osobnosti h2:eq(${e.id})`).text("");
+            }
+        });
+    });
+});
